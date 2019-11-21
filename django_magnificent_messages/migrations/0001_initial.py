b2db7ab80def4d9990af1c8fbc09aecd03ee2fe5
artem.vasin <artem.vasin>
2019-11-21 21:52:02 +0300

    chg: remove Django 1.11 support !minor

--- django_magnificent_messages/migrations/0001_initial.py
+++ django_magnificent_messages/migrations/0001_initial.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('auth', '0011_update_proxy_permissions'),
+        ('auth', '0001_initial'),
     ]
 
     operations = [
