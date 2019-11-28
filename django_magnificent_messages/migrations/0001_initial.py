2de81f8569f8006dd179965cd4747c7e3ace2b66
artem.vasin <artem.vasin>
2019-11-29 01:32:58 +0300

    fix: changed default inbox last_checked to 1900-01-01 !minor

--- django_magnificent_messages/migrations/0001_initial.py
+++ django_magnificent_messages/migrations/0001_initial.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('auth', '0001_init'),
+        ('auth', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
